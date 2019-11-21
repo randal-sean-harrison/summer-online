e2691476079d1c03db4f9deea76d97e9470c6b03
Randal Sean Harrison <rharriso@nd.edu>
2019-11-21 16:38:53 -0500

    updated latest

--- jquery-for-courses.js
+++ jquery-for-courses.js
@@ -5,6 +5,12 @@ jQuery(document).ready(function() {
 
   // Filter the page based on the typeahead search input
   jQuery("#search-text").on("keyup", function() {
+
+    jQuery("#csb-instructor").val("all");
+    jQuery("#csb-course-name").val("all");
+    jQuery("#csb-title").val("all");
+    jQuery("#csb-credits").val("all");
+
     var value = jQuery(this).val().toLowerCase();
     jQuery(".course-card").filter(function() {
       jQuery(this).toggle(jQuery(this).text().toLowerCase().indexOf(value) > -1)
@@ -90,43 +96,43 @@ jQuery(document).ready(function() {
     jQuery("#csb-instructor").append("<option value='" + allInstructors[i] + "'>" + allInstructors[i] + "</option>");
   }
 
-// --------------------------------------------------------
+  // --------------------------------------------------------
 
-// Get an array of all all credit hours
-var allCreditHours = [];
-jQuery('.cd-credits').each(function(i, e) {
-  allCreditHours.push(jQuery(e).text());
-});
-// sort the array
-allCreditHours.sort();
-// Dedupe the array
-allCreditHours = jQuery.unique(allCreditHours);
-// Add the instructors to the instructor ul element
-for (i = 0; i < allCreditHours.length; i++) {
-  jQuery("#csb-credits").append("<option value='" + allCreditHours[i] + "'>" + allCreditHours[i] + "</option>");
-}
+  // Get an array of all credit hours
+  var allCreditHours = [];
+  jQuery('.cd-credits').each(function(i, e) {
+    allCreditHours.push(jQuery(e).text());
+  });
+  // sort the array
+  allCreditHours.sort();
+  // Dedupe the array
+  allCreditHours = jQuery.unique(allCreditHours);
+  // Add the instructors to the instructor ul element
+  for (i = 0; i < allCreditHours.length; i++) {
+    jQuery("#csb-credits").append("<option value='" + allCreditHours[i] + "'>" + allCreditHours[i] + "</option>");
+  }
 
-// --------------------------------------------------------
+  // --------------------------------------------------------
 
-// Get an array of all all credit hours
-var allCourseName = [];
-jQuery('.cd-department').each(function(i, e) {
-  allCourseName.push(jQuery(e).text());
-});
-// sort the array
-allCourseName.sort();
-// Dedupe the array
-allCourseName = jQuery.unique(allCourseName);
-// Add the instructors to the instructor ul element
-for (i = 0; i < allCourseName.length; i++) {
-  jQuery("#csb-course-name").append("<option value='" + allCourseName[i] + "'>" + allCourseName[i] + "</option>");
-}
-
-// --------------------------------------------------------
-
-  // Get an array of all all courses
-  var allCourses = [];
+  // Get an array of all disciplines
+  var allCourseName = [];
   jQuery('.cd-department').each(function(i, e) {
+    allCourseName.push(jQuery(e).text());
+  });
+  // sort the array
+  allCourseName.sort();
+  // Dedupe the array
+  allCourseName = jQuery.unique(allCourseName);
+  // Add the instructors to the instructor ul element
+  for (i = 0; i < allCourseName.length; i++) {
+    jQuery("#csb-course-name").append("<option value='" + allCourseName[i] + "'>" + allCourseName[i] + "</option>");
+  }
+
+  // --------------------------------------------------------
+
+  // Get an array of course names
+  var allCourses = [];
+  jQuery('.course-title p').each(function(i, e) {
 
     allCourses.push(jQuery(e).text());
   });
@@ -185,7 +191,7 @@ for (i = 0; i < allCourseName.length; i++) {
     jQuery("#csb-credits").val("all");
 
     var courseValue = jQuery("#csb-title").val().toLowerCase();
-    jQuery(".course-card .course-title > p").filter(function() {
+    jQuery(".course-card").filter(function() {
       jQuery(this).toggle(jQuery(this).text().toLowerCase().indexOf(courseValue) > -1)
     });
 
@@ -233,6 +239,85 @@ for (i = 0; i < allCourseName.length; i++) {
 
   });
 
+  // Look into using a variable with replace() rather than literal strings (see next two lines)
+  // var dateObj = new Date();
+  // var thisYear = dateObj.getFullYear() + "-";
+
+  // Parse the date format to remove the 2019 and turn the hyphens into slashes
+  for (var l = 0; l < 2; l++) {
+    jQuery(".cd-date").text(function() {
+      return jQuery(this).text().replace("2019-", "");
+    });
+  }
+
+  // months
+  jQuery(".cd-date").text(function() {
+    return jQuery(this).text().replace("01-", "Jan ");
+  });
+  jQuery(".cd-date").text(function() {
+    return jQuery(this).text().replace("02-", "Feb ");
+  });
+  jQuery(".cd-date").text(function() {
+    return jQuery(this).text().replace("03-", "Mar ");
+  });
+  jQuery(".cd-date").text(function() {
+    return jQuery(this).text().replace("04-", "Apr ");
+  });
+  jQuery(".cd-date").text(function() {
+    return jQuery(this).text().replace("05-", "May ");
+  });
+  jQuery(".cd-date").text(function() {
+    return jQuery(this).text().replace("06-", "Jun ");
+  });
+  jQuery(".cd-date").text(function() {
+    return jQuery(this).text().replace("07-", "Jul ");
+  });
+  jQuery(".cd-date").text(function() {
+    return jQuery(this).text().replace("08-", "Aug ");
+  });
+  jQuery(".cd-date").text(function() {
+    return jQuery(this).text().replace("09-", "Sep ");
+  });
+  jQuery(".cd-date").text(function() {
+    return jQuery(this).text().replace("10-", "Oct ");
+  });
+  jQuery(".cd-date").text(function() {
+    return jQuery(this).text().replace("11-", "Nov ");
+  });
+  jQuery(".cd-date").text(function() {
+    return jQuery(this).text().replace("12-", "Dec ");
+  });
+
+  // Parse the string return from html for cd-time
+  jQuery('.cd-time').each(function(i, e) {
+    jQuery(this).html(jQuery(this).text());
+  });
+
+  // Remove Departmental Assigns from cd-time
+  jQuery('.cd-time span').each(function(i, e) {
+    jQuery(this).text(function() {
+      return jQuery(this).text().replace("DEPARTMENTAL ASSIGNS", "");
+    });
+  });
+
+  // Parse the string return from html for descriptions
+  jQuery('.decription-text').each(function(i, e) {
+    jQuery(this).html(jQuery(this).text());
+  });
+
+
+  jQuery(".fa-angle-up").removeClass("fa-angle-up").addClass("fa-caret-up").addClass("fa-2x");
+
+
+  // Remove the stupid capital P elements
+  // jQuery(".cd-date").text(function() {
+  //   return jQuery(this).text().replace("<P>", "");
+  // });
+
+  // Remove the stupid b (deprecated) bold elements
+  // jQuery(".cd-date").text(function() {
+  //   return jQuery(this).text().replace("<b>", "");
+  // });
 
 });
 // document.ready
